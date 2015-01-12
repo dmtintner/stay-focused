c00fa4d92b18293f02f02220545026687e12293a
DMTintner <dmtintner@gmail.com>
2015-01-12 09:57:42 +0200

    - close popup on successful alarm creation

--- popup.js
+++ popup.js
@@ -1,20 +1,27 @@
 var popup = {
-    $popup: document.getElementById('stayFocusedPopup'),
-    $form: document.getElementById('stayFocusedForm'),
-    $name: document.getElementById('taskName'),
-    $submit: document.getElementById('taskCreatorSubmit')
+    defaults: {
+        $popup: document.getElementById('stayFocusedPopup'),
+        $form: document.getElementById('stayFocusedForm'),
+        $name: document.getElementById('taskName')
+    },
+
+    close: function(){
+        window.close();
+    }
 };
 
+// TODO when you open popup show currently running alarms
+// TODO give option to cancel alarms
+// TODO score points for tasks completed and display score in popup???
+
 // Events
-popup.$form.addEventListener('submit', function (e) {
-    var name = popup.$name.value;
+popup.defaults.$form.addEventListener('submit', function(e) {
+    var name = popup.defaults.$name.value;
 
     e.preventDefault();
-    // send message with taskName
     chrome.runtime.sendMessage({taskName: name}, function(response) {
-        // TODO on success response from background.js, show success message
+        if (response.success){
+            popup.close();
+        }
     });
-
-    // TODO close popup or show success message
-    // chrome.browserAction.getPopup(object details, function callback)
 });
